9ec9a065afe94ae01fe7dd2b42619b31ef2caac8
Jennifer AUBINAIS <jennifer.aubinais@me.com>
2019-04-13 04:28:56 +0200

    Modify TestHIDAPI 04:30 13/04/2019

--- RaspberryPi/Test_hidapi.py
+++ RaspberryPi/Test_hidapi.py
@@ -17,11 +17,9 @@ if (count == 0):
 mcp2221a = hid.device()
 try:
     mcp2221a.open_path(hid.enumerate(0x04D8, 0x00DD)[0]["path"])
-    print("###############")
-    print("# Test HIDAPI #")
-    print("###############")
 except:
     print("#################################")
     print("#           Test HIDAPI         #")
     print("# ERROR : MCP2221 key not found #")
     print("#################################")
+    exit(1)
