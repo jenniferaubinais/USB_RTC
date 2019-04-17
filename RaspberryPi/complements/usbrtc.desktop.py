edd4c0009e4af1e6b6bdc706ed73fb9f522acbfe
Jennifer AUBINAIS <jennifer.aubinais@me.com>
2019-04-17 03:43:16 +0200

    Add autostart file 03:45 17/04/2019

---
+++ RaspberryPi/complements/usbrtc.desktop.py
@@ -0,0 +1,7 @@
+#####################################
+#    (c) Jennifer AUBINAIS          #
+#####################################
+[Desktop Entry]
+Type=Application
+Name=Clock
+Exec=sudo /usr/bin/python3 /home/pi/USB-RTC-Key/UPDATE_RaspberryPi.py
